38daa9761c693fadfeebccf81b9845bfef3b7863
viki <windviki@gmail.com>
2019-05-28 19:01:56 +0800

    Fixed NoMethodError: undefined method 'alias_method_chain' for XXX

--- lib/tagging_plugin/api_template_handler_patch.rb
+++ lib/tagging_plugin/api_template_handler_patch.rb
@@ -1,21 +1,10 @@
 module TaggingPlugin
   module ApiTemplateHandlerPatch
-    class << self
-      def included(base)
-        base.send(:extend, ClassMethods)
-        base.class_eval do
-          class << self
-            alias_method_chain :call, :api_replacement
-          end
-        end
-      end
-    end
-
     module ClassMethods
-      def call_with_api_replacement(template)
+      def call(template)
         template = replace_if(template, /views\/issues\/index.api.rsb$/, 'app/views/issues/index_with_tags.api.rsb')
         template = replace_if(template, /views\/issues\/show.api.rsb$/, 'app/views/issues/show_with_tags.api.rsb')
-        call_without_api_replacement(template)
+        super(template)
       end
 
       def replace_if(template, regexp, new_path)
@@ -32,4 +21,5 @@ module TaggingPlugin
   end
 end
 
-Redmine::Views::ApiTemplateHandler.send(:include, TaggingPlugin::ApiTemplateHandlerPatch)
+#Redmine::Views::ApiTemplateHandler.send(:include, TaggingPlugin::ApiTemplateHandlerPatch)
+Redmine::Views::ApiTemplateHandler.prepend(TaggingPlugin::ApiTemplateHandlerPatch::ClassMethods)
